f1c7cdee27dd6dce92031cb7c3fa4906ca3a4b29
marczenn <jamie.m.binns@gmail.com>
2016-08-19 12:49:12 -0600

    first commit

---
+++ index.ios.js
@@ -0,0 +1,75 @@
+import React from 'react';
+import {AppRegistry, MapView, Text, View, StyleSheet} from 'react-native';
+import Api from './src/api';
+
+var Weather = React.createClass({
+
+  getInitialState: function(){
+    return {
+      pin: {
+        latitude: 0,
+        longitude: 0,
+      },
+      city: '',
+      temperature: '',
+      description: ''
+    }
+  },
+
+  render: function(){
+
+    return (
+      <View style={styles.container}>
+        <MapView
+          annotations={[this.state.pin]}
+          style={styles.map}
+          onRegionChangeComplete={this.onRegionChangeComplete}
+        ></MapView>
+        <View style={styles.textContainer}>
+          <Text style={styles.text}>City: {this.state.city}</Text>
+          <Text style={styles.text}>{this.state.temperature}</Text>
+          <Text style={styles.text}>Description: {this.state.description}</Text>
+        </View>
+      </View>
+    );
+  },
+
+  onRegionChangeComplete: function(region){
+
+    this.setState({
+      pin: {
+        latitude: region.latitude,
+        longitude: region.longitude
+      }
+    });
+
+    Api(region.latitude, region.longitude)
+      .then(function(data){
+        console.log(data)
+        this.setState(data)
+      }.bind(this));
+  }
+});
+
+var styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+
+  map: {
+    flex: 5,
+    marginTop: 30
+  },
+
+  textContainer: {
+    flex: 3,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+
+  text: {
+    fontSize: 20
+  }
+});
+
+AppRegistry.registerComponent('weather', function(){return Weather});
---
+++ src/api.js
@@ -0,0 +1,24 @@
+var rootUrl = 'http://api.openweathermap.org/data/2.5/weather?APPID=b9b764974fcd990cadb1605e9fb20e53';
+
+var kelvinToF = function(kelvin){
+  return Math.round((kelvin-273.15) * 1.8 + 32) + ' ˚F'
+}
+
+module.exports = function(latitude, longitude){
+  var url = `${rootUrl}&lat=${latitude}&lon=${longitude}`;
+  console.log(url)
+  return fetch(url)
+    .then(function(response){
+      return response.json()
+    })
+    .then(function(json){
+      return {
+        city: json.name,
+        temperature: kelvinToF(json.main.temp),
+        description: json.weather[0].description
+      }
+    })
+    .catch(function(error){
+      console.log('the error', error)
+    })
+}
